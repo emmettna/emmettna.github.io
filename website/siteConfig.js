3ae8b2ea640ff57e4a1989ce49080764b7dff15c
emmett <emmett@doomoolmori.com>
2021-01-03 20:22:17 +0900

    Fix site confilg

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -23,14 +23,14 @@ const users = [
 const siteConfig = {
   title: 'Emmett Na | Tech Blog', // Title for your website.
   tagline: 'Emmett Na Tech Blog Posting About Computer Science And Data Science',
-  url: 'https://emmettna.io', // Your website URL
+  url: 'https://emmettna.github.io', // Your website URL
   baseUrl: '/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'emmettna',
+  projectName: 'emmettna.github.io',
   organizationName: 'emmettna',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
